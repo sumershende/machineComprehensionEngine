c1317eed20074f7113320962a2476812d0f99a47
mmurali5 <mmurali5@ncsu.edu>
2017-04-19 17:44:41 -0400

    GlOVe almost done

--- src/json2glove.py
+++ src/json2glove.py
@@ -1,15 +1,18 @@
 import json
-from tensorflowglove import tf_glove
+import tf_glove
+import matplotlib
+import random
 from nltk.tokenize import RegexpTokenizer
 
 ### Convert from JSON to glove vector
 
 ### Convert from JSON into TXT 
+random.seed(123)
 
 def read_json_data(json_data):
-	for i in range(len(json_data)):
+	for i in range(1):#len(json_data)):
 		#print(json_data[i]["title"])
-		for j in range(len(json_data[i]["paragraphs"])):
+		for j in range(1):#len(json_data[i]["paragraphs"])):
 			#print(json_data[i]["paragraphs"][j]["context"])
 			#print(tokenizer.tokenize(json_data[i]["paragraphs"][j]["context"]))
 			paragraphs.append(tokenizer.tokenize(json_data[i]["paragraphs"][j]["context"]))
@@ -27,7 +30,7 @@ def read_json_data(json_data):
 		
 tokenizer = RegexpTokenizer(r'\w+')
 json_file = "../train-v1.1.json"
-json_data_raw = open(json_file)
+json_data_raw = open(json_file,'r')
 json_data = json.load(json_data_raw)
 #print(json_data["data"][0]["title"])
 #print(json_data["data"][0]["paragraphs"][0]["context"])
@@ -37,5 +40,29 @@ questions = []
 answers = []
 qas_count = []
 read_json_data(json_data["data"])
-print(questions[0])
+glovemodel_paragraphs = tf_glove.GloVeModel(embedding_size=300, context_size=10)
+glovemodel_paragraphs.fit_to_corpus(paragraphs)
+glovemodel_paragraphs.train(num_epochs=100)
+
+glovemodel_paragraphs.generate_tsne("../tsne.png")
+#print(glovemodel_paragraphs.embeddings[glovemodel_paragraphs.id_for_word(paragraphs[0][0])])
+
+
+if(None):
+	glovemodel_para_count = tf_glove.GloVeModel(embedding_size=300, context_size=10)
+	glovemodel_para_count.fit_to_corpus(para_count)
+	glovemodel_para_count.train(num_epochs=10)
+
+	glovemodel_questions = tf_glove.GloVeModel(embedding_size=300, context_size=10)
+	glovemodel_questions.fit_to_corpus(questions)
+	glovemodel_questions.train(num_epochs=10)
+
+	glovemodel_answers = tf_glove.GloVeModel(embedding_size=300, context_size=10)
+	glovemodel_answers.fit_to_corpus(answers)
+	glovemodel_answers.train(num_epochs=10)
+
+	glovemodel_qas_count = tf_glove.GloVeModel(embedding_size=300, context_size=10)
+	glovemodel_qas_count.fit_to_corpus(qas_count)
+	glovemodel_qas_count.train(num_epochs=10)
+#print(questions[0])
 
