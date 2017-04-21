fe29535acc90ca24b5659a32309e61ff140fc9ae
ssn0602 <ssethur2@ncsu.edu>
2017-04-21 02:52:27 -0400

    DS_Analysis_Part

---
+++ src/txt2format.py
@@ -0,0 +1,51 @@
+read_data = open("temp.txt")
+
+import numpy as np
+val = []
+
+max_val = 0
+temp = 0
+loc = []
+tot = 0
+for txt in read_data:
+	tot = tot+1
+	if txt.find("?") >= 0:
+		st = txt.index("?") + 2
+		ed = len(txt)-1
+		if (ed-st) <= 200:
+			temp = txt[st:ed].count(" ")
+			val.append(temp)
+
+np_val = np.array(val)
+import matplotlib.pyplot as plt
+plt.hist(np_val,range=[0,40])
+plt.title("Histogram of Answer Length")
+plt.xlabel("Answer Length")
+plt.ylabel("Frequency")
+plt.show()
+
+
+
+if 0:
+
+	read_data = open("temp.txt")
+
+	import numpy as np
+	val = []
+	flag = 0
+	sum=0
+	for txt in read_data:
+		if txt.find("?") == -1:
+			sum = sum + len(txt.split())
+		else:
+			val.append(sum)
+			sum=0
+
+	np_val = np.array(val)
+	print(np_val)
+	import matplotlib.pyplot as plt
+	plt.hist(np_val,range=[0,500])
+	plt.title("Histogram of Paragraph Length")
+	plt.xlabel("Paragraph Length")
+	plt.ylabel("Frequency")
+	plt.show()
